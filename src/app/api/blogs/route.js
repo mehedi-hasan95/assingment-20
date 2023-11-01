c7ba81473071198bad3cf78fccc2d4be8bb00fcf
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-11-01 22:42:37 +0600

    Write post and get post done

---
+++ src/app/api/blogs/route.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+export async function POST(req, res) {
+    try {
+        const body = await req.json();
+        const posts = await prisma.posts.create({
+            data: body,
+        });
+        return NextResponse.json({ msg: "success", posts }, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ msg: "faill", error });
+    }
+}
+
+export async function GET(req, res) {
+    try {
+        const posts = await prisma.posts.findMany();
+        return NextResponse.json({ msg: "success", posts });
+    } catch (error) {
+        return NextResponse.json({ msg: "faill", error });
+    }
+}
---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const CreatePost = (params) => {
+    const router = useRouter();
+    //Convert title into slug
+    const slugify = (str) =>
+        str
+            .toLowerCase()
+            .trim()
+            .replace(/[^\w\s-]/g, "")
+            .replace(/[\s_-]+/g, "-")
+            .replace(/^-+|-+$&/g, "");
+    const handlePost = async (e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const short_desc = e.target.short_Desc.value;
+        const desc = e.target.desc.value;
+        const postImg = e.target.postImg.value;
+        const data = {
+            title,
+            slug: slugify(title),
+            short_desc,
+            desc,
+            postImg,
+            userId: parseInt(params.id),
+        };
+        try {
+            const response = await fetch("/api/blogs", {
+                method: "POST", // or 'PUT'
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            const result = await response.json();
+            if (result.msg === "success") {
+                router.push("/dashboard");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+    return (
+        <div>
+            <form onSubmit={handlePost} className="flex flex-col gap-y-4">
+                <div className="flex flex-col gap-y-2">
+                    <label htmlFor="title" className="font-semibold">
+                        Post Title:
+                    </label>
+                    <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        className="border border-black px-3 py-2 rounded-lg"
+                        placeholder="Post title"
+                    />
+                </div>
+                <div className="flex flex-col gap-y-2">
+                    <label htmlFor="short_Desc" className="font-semibold">
+                        Short Desc:
+                    </label>
+                    <textarea
+                        name="short_Desc"
+                        id="short_Desc"
+                        cols="30"
+                        rows="2"
+                        className="border border-black px-3 py-2 rounded-lg w-full"
+                        placeholder="Short Desc"
+                    ></textarea>
+                </div>
+                <div className="flex flex-col gap-y-2">
+                    <label htmlFor="desc" className="font-semibold">
+                        Desc:
+                    </label>
+                    <textarea
+                        name="desc"
+                        id="desc"
+                        cols="30"
+                        rows="5"
+                        className="border border-black px-3 py-2 rounded-lg w-full"
+                        placeholder="Your Content"
+                    ></textarea>
+                </div>
+                <div className="flex flex-col gap-y-2">
+                    <label htmlFor="postImg" className="font-semibold">
+                        Post Image:
+                    </label>
+                    <input
+                        type="text"
+                        name="postImg"
+                        id="postImg"
+                        className="border border-black px-3 py-2 rounded-lg"
+                        placeholder="Post Image CDN"
+                    />
+                </div>
+                <input
+                    type="submit"
+                    value="Post"
+                    className="text-xl font-semibold max-w-max bg-purple-400 px-5 py-2 rounded-lg text-white cursor-pointer"
+                />
+            </form>
+        </div>
+    );
+};
+
+export default CreatePost;
