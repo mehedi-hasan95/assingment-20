3e97df7149a5d7750b7b58580c1963493c0c592a
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-11-01 20:01:57 +0600

    Create category done

---
+++ src/app/api/categories/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+export async function POST(req, res) {
+    try {
+        const body = await req.json();
+        const categories = await prisma.categories.create({
+            data: body,
+        });
+        return NextResponse.json(
+            { msg: "success", categories },
+            { status: 201 }
+        );
+    } catch (error) {
+        return NextResponse.json({ msg: "faill", error });
+    }
+}
--- src/app/dashboard/categories/page.jsx
+++ src/app/dashboard/categories/page.jsx
@@ -1,7 +1,48 @@
-const page = () => {
+"use client";
+
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const CreateCategories = () => {
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    const slugify = (str) =>
+        str
+            .toLowerCase()
+            .trim()
+            .replace(/[^\w\s-]/g, "")
+            .replace(/[\s_-]+/g, "-")
+            .replace(/^-+|-+$&/g, "");
+    const handleCategories = async (e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const catImg = e.target.catImg.value;
+        const data = { title, catImg, slug: slugify(title) };
+        console.log(data);
+        try {
+            setLoading(true);
+            const response = await fetch("/api/categories", {
+                method: "POST", // or 'PUT'
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            const result = await response.json();
+            if (result.msg === "success") {
+                router.push("/dashboard");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
     return (
         <div>
-            <form className="flex flex-col gap-y-4">
+            <form onSubmit={handleCategories} className="flex flex-col gap-y-4">
                 <div className="flex flex-col gap-y-2">
                     <label htmlFor="catImg" className="font-semibold">
                         Category Image:
@@ -11,7 +52,8 @@ const page = () => {
                         name="catImg"
                         id="catImg"
                         className="border border-black px-3 py-2 rounded-lg"
-                        placeholder="Post Title"
+                        placeholder="Category Image"
+                        required
                     />
                 </div>
                 <div className="flex flex-col gap-y-2">
@@ -24,16 +66,29 @@ const page = () => {
                         id="title"
                         className="border border-black px-3 py-2 rounded-lg"
                         placeholder="Category Name"
+                        required
                     />
                 </div>
-                <input
-                    type="submit"
-                    value="Create"
-                    className="text-xl font-semibold max-w-max bg-purple-400 px-5 py-2 rounded-lg text-white cursor-pointer"
-                />
+                {loading ? (
+                    <button
+                        disabled
+                        className="text-xl font-semibold max-w-max bg-purple-400 px-5 py-2 rounded-lg text-white cursor-pointer disabled:cursor-not-allowed flex items-center gap-2"
+                    >
+                        Creating...{" "}
+                        <span className="animate-spin">
+                            <Loader2 size={24} />
+                        </span>
+                    </button>
+                ) : (
+                    <input
+                        type="submit"
+                        value="Create"
+                        className="text-xl font-semibold max-w-max bg-purple-400 px-5 py-2 rounded-lg text-white cursor-pointer"
+                    />
+                )}
             </form>
         </div>
     );
 };
 
-export default page;
+export default CreateCategories;
