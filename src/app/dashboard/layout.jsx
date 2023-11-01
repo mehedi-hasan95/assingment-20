0a5fbc992928e05e73cac1739aa45ccc0af1b489
unknown <mh.me.mehedi@gmail.com>
2023-11-01 10:06:00 +0600

    Dashboard layout done

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,29 @@
+import MainMenu from "@/components/menu/MainMenu";
+import Link from "next/link";
+
+const DashboardLayout = ({ children }) => {
+    return (
+        <div>
+            <MainMenu />
+            <div className="container mx-auto p-4 md:flex">
+                <div className="w-1/3 flex gap-5 flex-col">
+                    <Link
+                        href="/dashboard/categories"
+                        className="bg-slate-200 px-3 py-2 max-w-max "
+                    >
+                        Create Categories
+                    </Link>
+                    <Link
+                        href="/dashboard/posts"
+                        className="bg-slate-200 px-3 py-2 max-w-max "
+                    >
+                        Create Posts
+                    </Link>
+                </div>
+                <div className="w-2/3">{children}</div>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardLayout;
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,6 @@
-import MainMenu from "@/components/menu/MainMenu";
-
 const page = () => {
     return (
         <div>
-            <MainMenu />
             <div>Dashboard</div>
         </div>
     );
