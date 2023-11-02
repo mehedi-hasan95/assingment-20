4e5ef5d6911260ac2740bef0e39190be9b09758f
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-11-02 20:04:48 +0600

    Added top loader

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import FooterLayout from "@/components/footer/FooterLayout";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,12 @@ export default function RootLayout({ children }) {
                     (inter.className, "flex flex-col justify-between h-screen")
                 }
             >
+                <NextTopLoader
+                    color="#2563EB"
+                    height={3}
+                    speed={200}
+                    shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+                />
                 {children}
                 <FooterLayout />
             </body>
